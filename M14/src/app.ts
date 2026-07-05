42fb85e086128de6a74578b59d4109e708f4cd7e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-07-05 06:21:46 +0600

    feat: enhance /todos route to read from db.json and log request data

--- M14/src/app.ts
+++ M14/src/app.ts
@@ -1,7 +1,12 @@
 import express, {Application, Response, Request} from "express"
+import fs = require("fs")
+import path from "path"
+import process = require("process")
 
 const app : Application = express()
+app.use(express.json())
 
+const filePath=path.join(__dirname,"../db/db.json")
 
 app.get("/", (req : Request, res : Response)=>{
     res.send("Hello, World!")
@@ -9,11 +14,13 @@ app.get("/", (req : Request, res : Response)=>{
 
 app.get("/todos", (req : Request, res : Response)=>{
     console.log(req.query)
-
+    const data = fs.readFileSync(filePath, "utf8")
+    res.json(JSON.parse(data))
 })
 
 app.post("/todos/createTodo", (req : Request, res : Response)=>{
-    res.send("Todo created")
+    const {title,body} = req.body
+    console.log(req.body)
 })
 
 export default app; 
