06cc8c45f150a01f5cbd1fcb0f164a1e261d6ed1
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-07-09 04:55:40 +0600

    fix: update MongoDB connection URI to include database name

--- M14/src/server.ts
+++ M14/src/server.ts
@@ -4,7 +4,7 @@ import app from "./app";
 let server;
 const port = 5000
 
-const uri = "mongodb+srv://todosApp:XAIEbUd59WD0kzkh@postofsale.2psjhl2.mongodb.net/?appName=PostOfSale";
+const uri = "mongodb+srv://todosApp:XAIEbUd59WD0kzkh@postofsale.2psjhl2.mongodb.net/todosDB?appName=PostOfSale";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
