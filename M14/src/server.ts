93f85891cdd3b4d1f17bf2cf47cdfe07ed115b0a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-07-08 23:15:20 +0600

    feat: add MongoDB dependency and connection setup

--- M14/src/server.ts
+++ M14/src/server.ts
@@ -3,6 +3,16 @@ import app from "./app";
 let server;
 const port = 5000
 
+const uri = "mongodb+srv://todosApp:XAIEbUd59WD0kzkh@postofsale.2psjhl2.mongodb.net/?appName=PostOfSale";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 const bootstrap=async()=>{
     server= app.listen(port, ()=>{
     console.log(`App listing from ${port}`)
