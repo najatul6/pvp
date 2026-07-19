623d7160bf5b21c4f55c247df8adf261626b1888
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-07-19 06:26:49 +0600

    fix: update get and put routes for todos to handle async operations and improve functionality

--- M14/src/app/todos/todos.routes.ts
+++ M14/src/app/todos/todos.routes.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import fs from "fs";
 import path from "path";
 import { client } from "../../config/mongoDB";
+import { ObjectId } from "mongodb";
 
 const filePath = path.join(__dirname, "../../../db/db.json");
 export const todosRouter = express.Router();
@@ -23,15 +24,22 @@ todosRouter.post("/create-todo", async (req: Request, res: Response) => {
   res.status(201).json({ message: "Todo created successfully" });
 });
 
-todosRouter.get("/:id", (req: Request, res: Response) => {
-  
-})
+todosRouter.get("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const db = await client.db("todoDB")
+  const collection = await db.collection("todos")
+  const todo = await collection.findOne({ _id: new ObjectId(id) });
+  res.json({ todo });
+});
 
-todosRouter.put("/update-todo/:title", (req: Request, res: Response) => {
-  const { title } = req.body
-  console.log("Update todo")
-  res.send("Todo updated!")
-})
+todosRouter.put("/update-todo/:id", async(req: Request, res: Response) => {
+  const { id } = req.params;
+  const { title, description, priority } = req.body;
+  const db = await client.db("todoDB");
+  const collection = await db.collection("todos");
+  await collection.updateOne({ _id: new ObjectId(id) }, { $set: { title, description, priority } });
+  res.json({ message: "Todo updated successfully" });
+});
 
 todosRouter.delete("/delete-todo/:title", (req: Request, res: Response) => {
   const { title } = req.body;
