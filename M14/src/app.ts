318206129cac2bb4578f03e09bbd6c3b669d4c4f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-07-20 10:30:45 +0600

    fix improve code formatting and consistency in app.ts

--- M14/src/app.ts
+++ M14/src/app.ts
@@ -1,7 +1,7 @@
-import express, {Application, Response, Request, NextFunction} from "express"
-import {todosRouter} from "./app/todos/todos.routes"
+import express, { Application, Response, Request, NextFunction } from "express"
+import { todosRouter } from "./app/todos/todos.routes"
 import e from "express"
-const app : Application = express()
+const app: Application = express()
 
 
 app.use(express.json())
@@ -9,34 +9,47 @@ app.use(express.json())
 app.use("/todos", todosRouter)
 
 
-app.get("/", (req : Request, res : Response, next:NextFunction)=>{
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
     console.log({
         url: req.url,
         method: req.method,
-        header:req.header,
+        header: req.header,
         body: req.body
     })
     next()
 },
 
-async(req: Request, res: Response) => {
-    try {
-        res.send("Welcome to the API")
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).send("Internal Server Error")
-    }
+    async (req: Request, res: Response) => {
+        try {
+            res.send("Welcome to the API")
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).send("Internal Server Error")
+        }
+    })
+
+app.get("/error",
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            res.send("Welcome to the API")
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).send("Internal Server Error")
+        }
+    })
+
+app.all("*", (req: Request, res: Response) => {
+    res.status(404).json({
+        message: "Route not found"
+    })
 })
 
-app.get("/error", 
-async(req: Request, res: Response,next: NextFunction) => {
-    try {
-        res.send("Welcome to the API")
-        
-    } catch (error) {
-        console.log(error)
-        res.status(500).send("Internal Server Error")
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if(error){
+        console.error(error)
+        res.status(500).send("Internal Server Error from global error handler")
     }
 })
 
