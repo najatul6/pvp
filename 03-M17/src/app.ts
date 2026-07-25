7ad6e3bbdd2e9db237544f25fa111adc26efc115
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-07-26 01:35:59 +0600

    Add user CRUD routes and mount /user endpoint
    Introduces a new `user.controllers.ts` router with endpoints to create, list, retrieve, update, and delete users via the `User` model. Registers the router in `app.ts` under `/user` so the new user APIs are available in the application.

--- 03-M17/src/app.ts
+++ 03-M17/src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from 'express'
 import { NoteRoute } from './app/controllers/note.controllers';
+import { UserRoute } from './app/controllers/user.controllers';
 
 const app: Application = express();
 app.use(express.json());
 
 app.use("/notes", NoteRoute)
+app.use("/user", UserRoute)
 
 
 app.get('/', (req: Request, res: Response) => {
