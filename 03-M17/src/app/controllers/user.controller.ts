8f666561914547b3c7fa894e65bfae81211ca7a5
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-07-28 23:29:49 +0600

    Normalize formatting in user creation endpoint

--- 03-M17/src/app/controllers/user.controller.ts
+++ 03-M17/src/app/controllers/user.controller.ts
@@ -1,17 +1,17 @@
-import express,{ Request, Response } from "express";
+import express, { Request, Response } from "express";
 import { User } from "../models/user.model";
 
-export const UserRoute=express.Router()
+export const UserRoute = express.Router()
 
 // Create user Endpoint
-UserRoute.post("/create-user", async(req:Request, res:Response)=>{
-    const body=req.body;
-    const user=await User.create(body)
-    res.status(200).json({
-        success:true,
-        message:"User Create Successfully",
-        user
-    })
+UserRoute.post("/create-user", async (req: Request, res: Response) => {
+  const body = req.body;
+  const user = await User.create(body)
+  res.status(200).json({
+    success: true,
+    message: "User Create Successfully",
+    user
+  })
 })
 
 
