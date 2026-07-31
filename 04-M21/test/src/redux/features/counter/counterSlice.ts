ed1da0589c96e73eba897b8daa45a56a17c550a8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-07-31 23:12:44 +0600

    add actions in slice reducer

--- 04-M21/test/src/redux/features/counter/counterSlice.ts
+++ 04-M21/test/src/redux/features/counter/counterSlice.ts
@@ -5,7 +5,14 @@ const initialState = { count: 0 }
 const counterSlice = createSlice({
     name: "counter",
     initialState,
-    reducers: {}
+    reducers: {
+        increment: (state) => {
+            state.count += 1
+        },
+        decrement: (state) => {
+            state.count -= 1
+        }
+    }
 })
 
 export default counterSlice.reducer;
