75325f6738f8dff9a263f2c925c1ed4a3f55ec3f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-08-01 00:49:31 +0600

    Implement increment and decrement handlers in App component

--- 04-M21/test/src/App.tsx
+++ 04-M21/test/src/App.tsx
@@ -1,16 +1,28 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <>
       <section className="w-full mx-auto flex flex-col justify-center items-center space-y-6 pt-10">
         <h1 className="text-4xl font-bold">Counter With Redux</h1>
         <div className="w-9/12 flex justify-center items-center gap-6 p-10">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button onClick={handleIncrement} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Increment
           </button>
           <section>0</section>
-          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          <button onClick={handleDecrement} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
             Decrement
           </button>
         </div>
