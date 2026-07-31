2620c135317dc200ba46415325f1be88b6a2c301
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-08-01 00:58:45 +0600

    Add useSelector to retrieve count from Redux store in App component

--- 04-M21/test/src/App.tsx
+++ 04-M21/test/src/App.tsx
@@ -1,9 +1,11 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import { decrement, increment } from "./redux/features/counter/counterSlice";
+import type { RootState } from "./redux/store";
 
 function App() {
   const dispatch = useDispatch();
+  const { count } = useSelector((state: RootState) => state.counter);
 
   const handleIncrement = () => {
     dispatch(increment());
@@ -18,11 +20,19 @@ function App() {
       <section className="w-full mx-auto flex flex-col justify-center items-center space-y-6 pt-10">
         <h1 className="text-4xl font-bold">Counter With Redux</h1>
         <div className="w-9/12 flex justify-center items-center gap-6 p-10">
-          <button onClick={handleIncrement} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button
+            onClick={handleIncrement}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
             Increment
           </button>
-          <section>0</section>
-          <button onClick={handleDecrement} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          <section>
+            {count >= 1 && count <= 9 ? `0${count}` : count}
+          </section>
+          <button
+            onClick={handleDecrement}
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          >
             Decrement
           </button>
         </div>
