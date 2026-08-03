e208cb478916bdb55ac3f3098054047eb4937772
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-08-03 23:04:08 +0600

    Update App component to handle dynamic increment and decrement amounts

--- 04-M21/test/src/App.tsx
+++ 04-M21/test/src/App.tsx
@@ -6,34 +6,46 @@ function App() {
   const dispatch = useAppDispatch();
   const { count } = useAppSelector((state) => state.counter);
 
-  const handleIncrement = () => {
-    dispatch(increment());
+  const handleIncrement = (amount:number) => {
+    dispatch(increment(amount));
   };
 
-  const handleDecrement = () => {
-    dispatch(decrement());
+  const handleDecrement = (amount:number) => {
+    dispatch(decrement(amount));
   };
 
   return (
     <>
       <section className="w-full mx-auto flex flex-col justify-center items-center space-y-6 pt-10">
         <h1 className="text-4xl font-bold">Counter With Redux</h1>
-        <div className="w-9/12 flex justify-center items-center gap-6 p-10">
+        <div className="w-9/12 flex flex-col justify-center items-center p-10 space-y-5">
           <button
-            onClick={handleIncrement}
+            onClick={() => handleIncrement(5)}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Increment
+            Increment By 5
           </button>
-          <section>
-            {count >= 1 && count <= 9 ? `0${count}` : count}
-          </section>
+          <div className="flex justify-center items-center gap-6">
+            <button
+              onClick={() => handleIncrement(1)}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Increment
+            </button>
+            <section>{count >= 1 && count <= 9 ? `0${count}` : count}</section>
+            <button
+              onClick={() => handleDecrement(1)}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Decrement
+            </button>
+          </div>
           <button
-            onClick={handleDecrement}
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Decrement
-          </button>
+              onClick={() => handleDecrement(5)}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            > 
+              Decrement By 5
+            </button>
         </div>
       </section>
     </>
