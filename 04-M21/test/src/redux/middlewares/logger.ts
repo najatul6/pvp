9f644c7f3a264f6c507c44718859d82c5823bf02
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-08-05 02:56:53 +0600

    Refactor logger middleware to enhance state logging with grouped console output

--- 04-M21/test/src/redux/middlewares/logger.ts
+++ 04-M21/test/src/redux/middlewares/logger.ts
@@ -1,7 +1,10 @@
 const logger = (state) => (next) => (action) => {
-    console.log(state.getState())
-    console.log(action)
-    next(action)
+    console.group(action.type)
+    console.info("Prev state:", state.getState())
+    const result = next(action)
+    console.info("New state:", state.getState())
+    console.groupEnd()
+    return result
 }
 
 export default logger;
